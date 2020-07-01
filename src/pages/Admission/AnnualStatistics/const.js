3284329bb97d5ffe086cfba9d267579454b99067
lucasbui <locbq.cyclonedart@gmail.com>
2020-07-01 13:34:08 +0700

    admission statistics page

--- src/pages/Admission/AnnualStatistics/const.js
+++ src/pages/Admission/AnnualStatistics/const.js
@@ -12,3 +12,22 @@ export const contentList = [
     url: '/admission/annual-statistics',
   },
 ];
+
+export const tablData2015 = [
+  [
+    'Targeted number',
+    200,
+  ],
+  [
+    'Subjects',
+    'Math, Literature, English',
+  ],
+  [
+    'Admitee',
+    '~ 650',
+  ],
+  [
+    'Matriculation score (not include priority points)',
+    '21.25',
+  ],
+];
