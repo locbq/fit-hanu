3b2552742ee3f9fb940d169d9046e972122c678c
lucasbui <locbq.cyclonedart@gmail.com>
2020-12-04 09:13:56 +0700

    refactor login

--- src/theme/index.js
+++ src/theme/index.js
@@ -4,10 +4,12 @@ const theme = createMuiTheme({
   palette: {
     mainColor: '#fd8b4b',
     secondaryColor: '#231d1f',
+    dangerColor: '#fd4b64',
     white: '#ffffff',
     black: '#000000',
     underlineGrey: '#a5a5a5',
     borderGrey: '#eeeeee',
+    borderInput: '#cccccc',
     paragraphColor: '#333333',
     backgroundGrey: '#fafafa',
     arrowColor: 'rgba(255,255,255, 0.3)',
