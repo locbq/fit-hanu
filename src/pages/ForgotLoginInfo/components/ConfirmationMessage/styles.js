8935f9d578a6f89301525ff9f7afa8a37bd78c7b
locbq <locbq.cyclonedart@gmail.com>
2021-03-10 22:41:01 +0700

    refactor forgot login info

--- src/pages/ForgotLoginInfo/components/ConfirmationMessage/styles.js
+++ src/pages/ForgotLoginInfo/components/ConfirmationMessage/styles.js
@@ -1,4 +1,4 @@
-import { styled } from '@material-ui/core';
+import styled from '@material-ui/core/styles/styled';
 import { Link } from 'react-router-dom';
 
 import { Paragraph } from 'components/Headings';
